619843c265dbb6dbf7d7124d598cd2596775e2c0
sir-udenna <udennaboy@gmail.com>
2021-04-14 15:53:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+const num3 = 10;
+const num4 = 6;
+
+let multiply = num1 * num2
+
+const random = 10//Math.random(); Not working for some reason
+
+
+let mod = num3%num4
+
+let max = Math.max(1,2,3,4,5,6,7,20)
